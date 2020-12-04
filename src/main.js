894a297e3081c72af10680162bb589aa59bb96fe
coderwhy <coderwhy@why.local>
2020-12-04 11:09:05 +0800

    test jenkins

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ require('./app/database');
 
 const config = require('./app/config');
 
+console.log("test jenkins");
+
 app.listen(config.APP_PORT, () => {
   console.log(`服务器在${config.APP_PORT}端口启动成功~`);
 });
